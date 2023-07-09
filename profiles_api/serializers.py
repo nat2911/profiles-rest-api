9621dbd774cf59c8ab0803ed73ad8a00945cde7d
nat2911 <nandan.tendulkar@gmail.com>
2023-07-09 16:39:28 +0530

    Added profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 
 class HelloSerializer(serializers.Serializer):
     """
@@ -7,3 +9,36 @@ class HelloSerializer(serializers.Serializer):
     """
 
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializes user profile object
+    """
+
+    class Meta:
+        model = models.UserProfile
+        fields = ("id", "email", "name", "password")
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "style": {
+                    "input_type": "password",
+                },
+            },
+        }
+
+    # this will override create_user in UserProfileManager class
+    # in models.py file
+    def create(self, validated_data):
+        """
+        Create and return new user
+        """
+
+        user = models.UserProfile.objects.create_user(
+            email=validated_data["email"],
+            name=validated_data["name"],
+            password=validated_data["password"],
+        )
+
+        return user
