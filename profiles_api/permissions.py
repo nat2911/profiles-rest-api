d3fa4de10753f106cbbc882095f25c5b51683b5f
nat2911 <nandan.tendulkar@gmail.com>
2023-07-29 17:37:26 +0530

    Added user profile feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -14,3 +14,19 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return object.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """
+    Allow users to update their own status
+    """
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Check user is trying to update their own status
+        """
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
