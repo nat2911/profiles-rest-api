160c7bcd82418abc8b7a88a6e1f95c07be59f198
nat2911 <nandan.tendulkar@gmail.com>
2023-07-30 17:50:48 +0530

    Added yes to remove needrestart

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -10,7 +10,7 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
-sudo apt-get remove needrestart
+yes | sudo apt-get remove needrestart
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
